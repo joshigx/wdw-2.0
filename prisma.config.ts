4089a9f613feb2497819d2be74d19712232eb560
joshigx <joshi.gx@gmail.com>
2025-12-23 14:37:00 +0100

    removed seed path

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-    seed: `tsx prisma/seed.ts`,
+   // seed: `tsx prisma/seed.ts`,
   },
   datasource: {
     // url: Deno.env.get("DATABASE_URL"),
