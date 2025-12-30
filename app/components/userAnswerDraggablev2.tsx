954aea6fb959412c0169691ba4cf5af01ff7588b
joshigx <joshi.gx@gmail.com>
2025-12-30 11:43:56 +0100

    addedd clientonly componet and wraped dnd context in it, because of hydration missmatch error

--- app/components/userAnswerDraggablev2.tsx
+++ app/components/userAnswerDraggablev2.tsx
@@ -7,21 +7,32 @@ type DraggableProps = {
   id: string;
   children?: ReactNode;
   startPosition?: { x: number; y: number };
-  //onDragEnd?: (event: DragEndEvent) => void;
+  className?: string;
+  snapBack?: boolean
+  onDragEnd?: (event: DragEndEvent) => void;
 };
 
 export default function Draggable(props: DraggableProps) {
+  const { snapBack = false } = props;
   const [position, setPosition] = useState({
     x: (props.startPosition?.x || 0),
     y: (props.startPosition?.y || 0),
   });
 
   const onDragEnd = useCallback((event: DragEndEvent) => {
-    setPosition((prev) => ({
-      x: prev.x + event.delta.x,
-      y: prev.y + event.delta.y,
-    }));
-  }, []);
+
+
+
+    if (snapBack === false) {
+      setPosition((prev) => ({
+        x: prev.x + event.delta.x,
+        y: prev.y + event.delta.y,
+      }));
+    }
+
+    props.onDragEnd?.(event);
+
+  }, [snapBack, props.onDragEnd]);
 
   const { attributes, listeners, setNodeRef, transform } = useCustomDraggable({
     id: props.id,
@@ -37,18 +48,17 @@ export default function Draggable(props: DraggableProps) {
   };
 
   return (
-    <button
-      type="button"
+    <div
       ref={setNodeRef}
       style={style}
+      className={props.className}
+
+
+
       {...listeners}
       {...attributes}
-      className="text-white bg-brand box-border bg-amber-700 border border-transparent 
-         hover:bg-brand-strong hover:bg-amber-900 
-         focus:ring-4 focus:ring-brand-medium 
-         shadow-xs font-medium leading-5 rounded-full text-sm px-4 py-2.5 focus:outline-none"
     >
       {props.children}
-    </button>
+    </div>
   );
 }
--- app/routes/game.tsx
+++ app/routes/game.tsx
@@ -1,10 +1,9 @@
-// dnd.tsx
-
 import { DndContext } from "@dnd-kit/core";
 import type { Route } from "./+types/game.ts";
 
 import Draggable from "../components/userAnswerDraggablev2.tsx";
 import users from "./api/testUsers.json" with { type: "json" };
+import { ClientOnly } from "../components/ClientOnly.tsx";
 
 interface Room {
   id: string;
@@ -18,14 +17,14 @@ interface User {
   answer: string;
 }
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "wdw Game" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
-export function loader({}: Route.LoaderArgs) {
+export function loader({ }: Route.LoaderArgs) {
   const typedUsers: User[] = users as User[];
   const _lenght = typedUsers.length;
 
@@ -37,16 +36,26 @@ export default function Game({ loaderData }: Route.ComponentProps) {
   const startPos = { x: 70, y: 70 };
 
   return (
-    <DndContext>
-      {users.map((user: User) => (
-        <Draggable
-          startPosition={startPos}
-          key={user.id}
-          id={user.id}
-        >
-          Anwort: {user.answer}
-        </Draggable>
-      ))}
-    </DndContext>
+    <ClientOnly>
+      <DndContext>
+        {users.map((user: User) => (
+          <Draggable
+            startPosition={startPos}
+            key={user.id}
+            id={user.id}
+            snapBack={false}
+            onDragEnd={(event) => (console.log("Test " + event.delta.x + event.delta.y))}
+            className="text-white bg-brand box-border 
+  bg-amber-600 border border-transparent 
+  hover:bg-brand-strong hover:bg-amber-900 
+  focus:ring-4 focus:ring-brand-medium shadow-xs 
+  focus:outline-none
+  font-medium leading-5 rounded-full text-sm px-4 py-2.5 "
+          >
+            Anwort: {user.answer}
+          </Draggable>
+        ))}
+      </DndContext>
+    </ClientOnly>
   );
 }
