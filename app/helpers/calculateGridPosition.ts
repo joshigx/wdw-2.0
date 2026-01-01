a310790b40abb4a48ea1ab337bebf9e6a3af2eee
joshigx <joshi.gx@gmail.com>
2026-01-01 19:00:57 +0100

    responsive collums size

--- app/helpers/calculateGridPosition.ts
+++ app/helpers/calculateGridPosition.ts
@@ -13,7 +13,7 @@ interface Viewport {
 export type DraggablePositon = Record<string, { x: number; y: number }>;
 
 export const GRID_CONFIG = {
-  columns: 4,
+  // columns: 2,
   cellWidth: 200,
   cellHeight: 100,
   marginX: 20,
@@ -32,24 +32,23 @@ function calculateGridPosition(
 ) {
   //aktuelle Spalte für den aktuelle Index
   //Bsp: 10 Elemente, 3 Spalten: (0-index): 0%3=0 (Spalte 1) 1%3=0 (Spalte 1) 3 (4. ELement) % 3 = 0 (wieder 1. Spalte aber schon in der 2. Zeile)
+
   const col = index % columns;
   const row = Math.floor(index / columns);
-  const numberOfRows: number = Math.floor((numberOfUsers - 1) / columns) + 1;
 
+  const numberOfRows: number = Math.floor((numberOfUsers - 1) / columns) + 1;
 
   //basis koordinaten für jede karte (der punkt ist immer oben links bei dem jeweiligen container)
   const baseX = col * (cellWidth + marginX);
   const baseY = row * (cellHeight + marginY);
 
-
   const totalGridWidth = cellWidth * columns + marginX * (columns - 1);
   const totalGridHeight = cellHeight * numberOfRows +
     marginY * (numberOfRows - 1);
 
-    //DIe Anzahl an Pixeln, die man das Element verschieben muss, damit alle ganz mittig sind, sowohl x und y
+  //DIe Anzahl an Pixeln, die man das Element verschieben muss, damit alle ganz mittig sind, sowohl x und y
   const offsetX = (viewport.width - totalGridWidth) / 2;
   const offsetY = (viewport.height - totalGridHeight) / 2;
-
 
   return {
     x: baseX + offsetX,
@@ -57,18 +56,21 @@ function calculateGridPosition(
   };
 }
 
+
 export default function getInitialPositions(
   users: User[],
   viewport: Viewport,
 ): DraggablePositon {
   const initialPositions: DraggablePositon = {};
+  const numberOfUsers = users.length;
+  const columns = (numberOfUsers > 6) ? 3 : 2;
 
-  users.forEach((user, index, users) => {
+  users.forEach((user, index) => {
     initialPositions[user.id] = calculateGridPosition(
       index,
       viewport,
-      users.length,
-      GRID_CONFIG.columns,
+      numberOfUsers,
+      columns,
       GRID_CONFIG.cellWidth,
       GRID_CONFIG.cellHeight,
       GRID_CONFIG.marginX,
