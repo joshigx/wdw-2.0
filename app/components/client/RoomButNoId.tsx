24da25257c024e723b66bc31fab6a92b50c002d5
joshigx <joshi.gx@gmail.com>
2026-01-06 18:16:09 +0100

    implemented creation of user, if room id is right

--- app/components/client/RoomButNoId.tsx
+++ app/components/client/RoomButNoId.tsx
@@ -1,4 +1,6 @@
+import { Form } from "react-router";
 import type { RoomModel } from "../../generated/prisma/models/Room.ts";
+import Button, { Color } from "../Button.tsx";
 
 interface RoomButNoIdProps {
   room: RoomModel | null;
@@ -11,10 +13,18 @@ export default function RoomButNoId(props: RoomButNoIdProps) {
     <div>
       {!roomIsValid ? "ungültiger Raum" : (
         <div>
+
+
           "gültiger Raum"
           {
             //hier User-Erstellungslogik einbauen
           }
+
+          <Form method="post">
+            <input className="bg-amber-200 text-black" type="text" name="userName" />
+            
+            <Button bgColor={Color.GREEN} type="submit">Submit</Button>
+          </Form>
         </div>
       )}
     </div>
--- app/routes/client.tsx
+++ app/routes/client.tsx
@@ -5,12 +5,39 @@ import type { Route } from "../../.react-router/types/app/routes/+types/client.t
 import NoRoomYet from "../components/client/NoRoomYet.tsx";
 import prisma from "../lib/prisma.ts";
 import RoomButNoId from "../components/client/RoomButNoId.tsx";
+import type { UserModel } from "../generated/prisma/models.ts";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
   ];
+}
+
+
+
+export async function action({
+  request, params
+}: Route.ActionArgs) {
+  let user: UserModel | null = null;
+  const formData: FormData = await request.formData();
+
+  const name = formData.get("userName") as string || "";
+
+  //eventuell noch sicherheitsabfrage, dass roomId wirklich gülitg ist, obwohl es das sein müsste
+  if (name && params.roomId! ) {
+    user = await prisma.user.create({
+      data: {
+        name: name,
+        locationId: params.roomId!,
+      },
+    });
+  }
+
+  console.log("User angelegt: " + user);
+  
+
+  return { user }
 }
 
 export async function loader(props: Route.LoaderArgs) {
